a4b66f0f223ad686570d22d57b55aac2f00ca7d8
Ledanghuy <ledanghuy91@gmail.com>
2023-03-01 15:41:05 +0700

    Finish_Exec_Textbox_And_TextArea

--- src/Webdriver/Topic_09_Textbox_and_TextArea.java
+++ src/Webdriver/Topic_09_Textbox_and_TextArea.java
@@ -1,10 +1,15 @@
 package Webdriver;
 
+
+
+import java.awt.Desktop.Action;
 import java.util.List;
 import java.util.Random;
+import java.util.ResourceBundle.Control;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriver.Navigation;
 import org.openqa.selenium.WebDriver.Options;
@@ -14,6 +19,7 @@ import org.openqa.selenium.WebDriver.Window;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -28,21 +34,27 @@ public class Topic_09_Textbox_and_TextArea {
 	
 	String Employee_ID;
 	
+	String passport_Number ="40517-402-96-7202";
+	String comment_input= "This is generated data \n of real people";
+	Actions control__action;
+	
+	
+	
 
 	@BeforeClass
 	public void beforeClass() {
 		if (osName.contains("Windows")) {
-			System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
+			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
 		} else {
-			System.setProperty("webdriver.chrome.driver", projectPath + "/browserDrivers/chromedriver");
+			System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
 		}
-			
-		driver = new ChromeDriver();
+		driver = new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		
 		rand_1= new Random();
 		Employee_ID=String.valueOf(rand_1.nextInt(999999));
+		control__action = new Actions (driver);
 		
 		
 	}
@@ -70,50 +82,76 @@ public class Topic_09_Textbox_and_TextArea {
 		driver.findElement(By.xpath("//a[text()='Add Employee']")).click();
 		SleepInSecond(3);
 		
-		driver.findElement(By.name("firstname")).sendKeys("Le Dang");
-		driver.findElement(By.name("lastname")).sendKeys("Huy");
+		driver.findElement(By.name("firstName")).sendKeys("Le Dang");
+		driver.findElement(By.name("lastName")).sendKeys("Huy");
 		
+		
+		
+		
+		driver.findElement(By.xpath("//label[text()='Employee Id']/parent::div/following-sibling::div/input")).sendKeys(Keys.chord(Keys.CONTROL,"a"));
+		SleepInSecond(1);
+		driver.findElement(By.xpath("//label[text()='Employee Id']/parent::div/following-sibling::div/input")).sendKeys(Keys.chord(Keys.DELETE));
 		driver.findElement(By.xpath("//label[text()='Employee Id']/parent::div/following-sibling::div/input")).sendKeys(Employee_ID);
+		SleepInSecond(3);
 		
-		
-		driver.findElement(By.xpath("//p[text()='Create Login Details']/parent::div//input")).click();
-		
+		driver.findElement(By.xpath("//p[text()='Create Login Details']/parent::div//span")).click();
+		SleepInSecond(3);
 		
 		driver.findElement(By.xpath("//label[text()='Username']/parent::div/following-sibling::div/input")).sendKeys("afc"  + Employee_ID);
-		driver.findElement(By.xpath("//label[text()='Password']/parent::div/following-sibling::div/input")).sendKeys("Password123!!!");
-		
+		driver.findElement(By.xpath("//label[text()='Password']/parent::div/following-sibling::div/input")).sendKeys("Password123!!!");	
 		driver.findElement(By.xpath("//label[text()='Confirm Password']/parent::div/following-sibling::div/input")).sendKeys("Password123!!!");
+		driver.findElement(By.xpath("//button[contains(string(),'Save')]")).click();
+		//driver.findElement(By.xpath("//button[contains(.,'Save')]")).click();
+		
+		SleepInSecond(10);
+		
+		//a[text()='Immigration']
+		
+		//a[contains(.,'Immigration')]
+		
+		Assert.assertEquals(driver.findElement(By.name("firstName")).getAttribute("value"), "Le Dang");
+		Assert.assertEquals(driver.findElement(By.name("lastName")).getAttribute("value"), "Huy");
 		
 		
-		//driver.findElement(By.xpath(Employee_ID))
+		Assert.assertEquals(driver.findElement(By.xpath("//label[text()='Employee Id']/parent::div/following-sibling::div/input")).getAttribute("value"),Employee_ID);
+		
+		
+		driver.findElement(By.xpath("//a[text()='Immigration']")) .click();
+		SleepInSecond(6);
+		
+		
+		driver.findElement(By.xpath("//h6[text()='Assigned Immigration Records']/ following-sibling::button")) .click();
+		driver.findElement(By.xpath("//label[text()='Number']/parent::div/following-sibling::div/input")).sendKeys(passport_Number);
+		driver.findElement(By.xpath("//label[text()='Comments']/parent::div/following-sibling::div/textarea")).sendKeys(comment_input);
+		driver.findElement(By.xpath("//button[text()=' Save ']")).click();
+		
+		SleepInSecond(6);
+		
+		driver.findElement(By.cssSelector("i.bi-pencil-fill")).click();
+		SleepInSecond(3);
 		
 		
 		
 		
+		Assert.assertEquals(driver.findElement(By.xpath("//label[text()='Number']/parent::div/following-sibling::div/input")).getAttribute("value"), passport_Number);
+		Assert.assertEquals(driver.findElement(By.xpath("//label[text()='Comments']/parent::div/following-sibling::div/textarea")).getAttribute("value"), comment_input);
 		
+		driver.findElement(By.cssSelector("p.oxd-userdropdown-name")).click();
+	
+		driver.findElement(By.xpath("//a[text()='Logout']")).click();
 		
+		SleepInSecond(3);
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-	 
-		
-		
-		
-		
-		
-		
-		
-		
+		driver.findElement(By.name("username")).sendKeys("afc" +Employee_ID);
+		driver.findElement(By.name("password")).sendKeys("Password123!!!");
 
+		//csss 
+		driver.findElement(By.cssSelector("button.orangehrm-login-button")).click();	
+
+
+		SleepInSecond(2);
 		
+	
 	}
 	@Test
 	public void TC_02() {
